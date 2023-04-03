3ae434386074611c9a99f2bf569cf5c7c500cda1
Olena Holovata <golovatajaelena@gmail.com>
2023-04-03 21:17:00 +0300

    New Commit

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Student student = new Student();
-        String newValue = String.format("\"~~~%d~~~\"%n", student.age);
-        System.out.println(newValue);
-        student.age();
-    }
-}
--- src/Student.java
+++
@@ -1,9 +0,0 @@
-public class Student {
-
-    public int age = 32;
-
-    public String age() {
-        String updatedValue = String.format("\"---%d---\"%n", age);
-        System.out.println(updatedValue);
-        return updatedValue;}
-}
